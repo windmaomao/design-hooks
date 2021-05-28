590a7726a287896fda96c1589cccf92551604c69
Fang Jin <windmaomao@gmail.com>
2021-05-28 10:13:04 -0400

    add a react test

---
+++ test/react.test.js
@@ -0,0 +1,20 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+const App = () => {
+  return (
+    <div data-testid="test">
+      Hello World
+    </div>
+  )
+}
+
+test('render', () => {
+  const { getByTestId } = render(<App />)
+  const div = getByTestId('test')
+  expect(div.textContent).toBe("Hello World")
+})
