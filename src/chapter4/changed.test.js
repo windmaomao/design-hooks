30cd800b9ff61b6d0499baf7c5a474809695ca60
Fang Jin <windmaomao@gmail.com>
2021-06-15 13:53:03 -0400

    add state changed part II

--- src/chapter4/changed.test.js
+++ src/chapter4/changed.test.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {render, screen, fireEvent, waitFor} from '@testing-library/react'
+import {render, screen, fireEvent, waitFor, act} from '@testing-library/react'
 
 describe('Use State', () => {
   test('when the state changed, part I', async () => {
@@ -40,14 +40,17 @@ describe('Use State', () => {
     const Title = ({ log, r }) => {
       const [value, setValue] = useState(0)
       const onClick = () => {
-        setValue(value + 1)
-        log(value)
+        setTimeout(() => {
+          setValue(value + 1)
+          log(value + 1)
+        }, 3000)
       }
 
       r(value)
       return <button role="text" onClick={onClick} />
     }    
     
+    jest.useFakeTimers('legacy')
     const log = jest.fn(), r = jest.fn()
     const _t = () => screen.getByRole('text')
 
@@ -58,16 +61,27 @@ describe('Use State', () => {
     // first click
     fireEvent.click(_t())
     await waitFor(_t)
-    expect(r).toHaveReturnedTimes(2)
-    expect(log).toHaveBeenLastCalledWith(0)
-    expect(r).toHaveBeenLastCalledWith(1)
+    expect(r).toHaveReturnedTimes(1)
+    expect(r).toHaveBeenLastCalledWith(0)
 
     // second click
     fireEvent.click(_t())
     await waitFor(_t)
-    expect(r).toHaveReturnedTimes(3)
+    expect(r).toHaveReturnedTimes(1)
+    expect(r).toHaveBeenLastCalledWith(0)
+
+    // third click
+    fireEvent.click(_t())
+    await waitFor(_t)
+    expect(r).toHaveReturnedTimes(1)
+    expect(r).toHaveBeenLastCalledWith(0)
+
+    // wait for the end
+    waitFor(() => {
+      jest.runAllTimers() 
+      expect(r).toHaveReturnedTimes(4)
+    })
     expect(log).toHaveBeenLastCalledWith(1)
-    expect(r).toHaveBeenLastCalledWith(2)
   })  
  
 })
