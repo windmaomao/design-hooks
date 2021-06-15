e779041ff4a9160e352f667b0cfd12c78b83ebd7
Fang Jin <windmaomao@gmail.com>
2021-06-15 16:23:56 -0400

    add useEffect implementation

---
+++ src/chapter5/implementation.test.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import {render, screen, fireEvent, waitFor} from '@testing-library/react'
+
+const CurrentComponent = { hooks: {} }
+
+function getHook(id) {
+  const c = CurrentComponent
+  if (c.hooks[id] === undefined) 
+    c.hooks[id] = {}
+  return c.hooks[id]
+}
+
+function useEffect(callback, deps) {
+  const c = CurrentComponent
+  const h = getHook(c.hookId++)
+  if (isChanged(h.deps, deps)) {			   	    // ➊ 
+    h.callback = callback
+    h.deps = deps
+    c.effects.push(h)							            // ➋
+  }
+  return                                      // ➌ 
+}
+
+function isChanged(deps, nextDeps) {
+  return (
+    !deps ||								                 // ➍
+    deps.length !== nextDeps.length ||       // ➎
+    nextDeps.some((v, i) => v !== deps[i])   // ➏
+  )
+}
+
+function beforeRender() {
+  CurrentComponent.effects = []
+  CurrentComponent.hookId = 0 
+}
+
+function runEffects() {
+  CurrentComponent.effects.forEach(h => {
+    h.teardown = h.callback()
+  })
+}
+
+describe('Use Effect', () => {
+  test('our implementation', async () => {
+    const Title = ({ text, log, r }) => {
+      useEffect(() => {
+        log()
+      }, [text])
+
+      r()
+      return <div data-testid="test">{text}</div>
+    }    
+    
+    const log = jest.fn(), r = jest.fn()
+    const _t = () => screen.getByRole('text')
+    let text = "a"
+  
+    // text "a"
+    beforeRender()
+    const { rerender } = render(<Title text={text} log={log} r={r} />)
+    runEffects()
+    await waitFor(() => expect(r).toHaveReturnedTimes(1))
+    expect(log).toHaveReturnedTimes(1)
+    
+    // text "a" 
+    beforeRender()
+    rerender(<Title text={text} log={log} r={r} />)
+    runEffects()
+    await waitFor(() => expect(r).toHaveReturnedTimes(2))
+    expect(log).toHaveReturnedTimes(1)
+
+    // text "b"
+    text = "b"
+    beforeRender()
+    rerender(<Title text={text} log={log} r={r} />)
+    runEffects()
+    await waitFor(() => expect(r).toHaveReturnedTimes(3))
+    expect(log).toHaveReturnedTimes(2)
+  })
+ 
+})
--- src/chapter5/useEffect.test.js
+++ src/chapter5/useEffect.test.js
@@ -6,6 +6,7 @@ describe('Use Effect', () => {
     const Title = ({ text, log, r }) => {
       const [count, setCount] = useState(0)
       useEffect(() => {
+        log()
         setCount(0)
       }, [text])
       const onClick = () => { setCount(count + 1) }
@@ -21,21 +22,25 @@ describe('Use Effect', () => {
     const { rerender } = render(<Title text="a" log={log} r={r} />)
     await waitFor(() => expect(r).toHaveReturnedTimes(1))
     expect(r).toHaveBeenLastCalledWith(0)
+    expect(log).toHaveReturnedTimes(1)
     
     // first click
     fireEvent.click(_t())
     await waitFor(() => expect(r).toHaveReturnedTimes(2))
     expect(r).toHaveBeenLastCalledWith(1)
+    expect(log).toHaveReturnedTimes(1)
 
     // second click
     fireEvent.click(_t())
     await waitFor(() => expect(r).toHaveReturnedTimes(3))
     expect(r).toHaveBeenLastCalledWith(2)
+    expect(log).toHaveReturnedTimes(1)
 
     // text "b"
     rerender(<Title text="b" log={log} r={r} />)
     await waitFor(() => expect(r).toHaveReturnedTimes(5))
     expect(r).toHaveBeenLastCalledWith(0)
+    expect(log).toHaveReturnedTimes(2)
 
   })
  
