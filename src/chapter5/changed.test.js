a71f5d2617e6f747ba91fd7778a4b69e6f571d39
Fang Jin <windmaomao@gmail.com>
2021-06-15 20:37:07 -0400

    add use effect changed fix

--- src/chapter5/changed.test.js
+++ src/chapter5/changed.test.js
@@ -6,10 +6,9 @@ describe('Use Effect', () => {
     const Title = ({ log, r }) => {
       const [count, setCount] = useState(0)
       useEffect(() => {
-        setTimeout(() => {
-          console.log('called')
+        setInterval(() => {
           log(count)
-        }, 3000)
+        }, 1000)
       }, [])
       const onClick = () => { setCount(count + 1) }
 
@@ -21,7 +20,6 @@ describe('Use Effect', () => {
     const log = jest.fn(), r = jest.fn()
     const _t = () => screen.getByRole('text')
 
-    // text "a"
     const { rerender } = render(<Title log={log} r={r} />)
     await waitFor(() => expect(r).toHaveReturnedTimes(1))
     expect(r).toHaveBeenLastCalledWith(0)
@@ -40,24 +38,65 @@ describe('Use Effect', () => {
     expect(log).toHaveReturnedTimes(0)
 
     // wait for 3 seconds
-    await waitFor(() => {
-      jest.advanceTimersByTime(3000)
-      expect(log).toHaveReturnedTimes(1)
-    })
+    jest.advanceTimersByTime(1000)
+    await waitFor(() => expect(log).toHaveReturnedTimes(1))
+    expect(log).toHaveBeenLastCalledWith(0)
+    jest.advanceTimersByTime(1000)
+    await waitFor(() => expect(log).toHaveReturnedTimes(2))
+    expect(log).toHaveBeenLastCalledWith(0)
+    jest.advanceTimersByTime(1000)
+    await waitFor(() => expect(log).toHaveReturnedTimes(3))
+    expect(log).toHaveBeenLastCalledWith(0)
+  })
+ 
+  test('fix with setInterval', async () => {
+    const Title = ({ log, r }) => {
+      const [count, setCount] = useState(0)
+      useEffect(() => {
+        const id = setInterval(() => {
+          log(count)
+        }, 1000)
+        return () => {
+          clearInterval(id)
+        }
+      }, [count])
+      const onClick = () => { setCount(count + 1) }
 
-    await waitFor(() => {
-      jest.advanceTimersByTime(3000)
-      expect(log).toHaveReturnedTimes(1)
-    })
+      r(count)
+      return <button role="text" onClick={onClick} />
+    }    
     
+    jest.useFakeTimers('legacy')
+    const log = jest.fn(), r = jest.fn()
+    const _t = () => screen.getByRole('text')
 
-    // wait for 3 second
-    // waitFor(() => {
-    //   jest.runAllTimers()
-    //   return expect(log).toHaveReturnedTimes(1)    
-    // })
-    // console.log('dd')
+    const { rerender } = render(<Title log={log} r={r} />)
+    await waitFor(() => expect(r).toHaveReturnedTimes(1))
+    expect(r).toHaveBeenLastCalledWith(0)
+    expect(log).toHaveReturnedTimes(0)
 
+    // first click
+    fireEvent.click(_t())
+    await waitFor(() => expect(r).toHaveReturnedTimes(2))
+    expect(r).toHaveBeenLastCalledWith(1)
+    expect(log).toHaveReturnedTimes(0)
+
+    // second click
+    fireEvent.click(_t())
+    await waitFor(() => expect(r).toHaveReturnedTimes(3))
+    expect(r).toHaveBeenLastCalledWith(2)
+    expect(log).toHaveReturnedTimes(0)
+
+    // wait for 3 seconds
+    jest.advanceTimersByTime(1000)
+    await waitFor(() => expect(log).toHaveReturnedTimes(1))
+    expect(log).toHaveBeenLastCalledWith(2)
+    jest.advanceTimersByTime(1000)
+    await waitFor(() => expect(log).toHaveReturnedTimes(2))
+    expect(log).toHaveBeenLastCalledWith(2)
+    jest.advanceTimersByTime(1000)
+    await waitFor(() => expect(log).toHaveReturnedTimes(3))
+    expect(log).toHaveBeenLastCalledWith(2)
   })
  
 })
