2190d52169783e9fbc4b0da389f911878cad5e20
Fang Jin <windmaomao@gmail.com>
2021-06-17 16:34:20 -0400

    add useMemoo type and search

---
+++ src/chapter6/useMemo.test.js
@@ -0,0 +1,155 @@
+import React, { useState, useMemo } from 'react'
+import {render, screen, fireEvent, waitFor} from '@testing-library/react'
+
+const fn = jest.fn()
+describe('Use Memo', () => {
+  test('type and search without useMemo', async () => {
+    const Title = ({ log, r }) => {
+      const [text, setText] = React.useState('')
+      const [search, setSearch] = React.useState('')
+      const handleType = e => { setText(e.target.value) }
+      const handleSearch = () => { setSearch(text) }
+
+      log()
+      const matched = fn()
+      r()
+      return (
+        <>
+          <input role="text" value={text} onChange={handleType} />
+          <button role="search" onClick={handleSearch}>Search</button>
+          {matched}          
+        </>
+      )
+    }    
+    
+    const log = jest.fn(), r = jest.fn()
+    const _t = () => screen.getByRole('text')
+    const _s = () => screen.getByRole('search')
+
+    render(<Title log={log} r={r} />)
+    await waitFor(() => expect(r).toHaveReturnedTimes(1))
+    expect(log).toHaveReturnedTimes(1)
+    
+    // first typing session
+    fireEvent.change(_t(), { target: { value: "k" } })
+    fireEvent.change(_t(), { target: { value: "kk" } })
+    fireEvent.change(_t(), { target: { value: "kkk" } })
+    fireEvent.change(_t(), { target: { value: "kkkk" } })
+    await waitFor(() => expect(r).toHaveReturnedTimes(5))
+    expect(log).toHaveReturnedTimes(5)
+
+    // first search click
+    fireEvent.click(_s())
+    await waitFor(() => expect(r).toHaveReturnedTimes(6))
+    expect(log).toHaveReturnedTimes(6)
+
+    // second typing session
+    fireEvent.change(_t(), { target: { value: "k" } })
+    fireEvent.change(_t(), { target: { value: "kk" } })
+    await waitFor(() => expect(r).toHaveReturnedTimes(8))
+    expect(log).toHaveReturnedTimes(8)
+    
+    // second search click
+    fireEvent.click(_s())
+    await waitFor(() => expect(r).toHaveReturnedTimes(9))
+    expect(log).toHaveReturnedTimes(9)
+  })
+
+  test('type and search with useMemo', async () => {
+    const Title = ({ log, r }) => {
+      const [text, setText] = React.useState('')
+      const [search, setSearch] = React.useState('')
+      const handleType = e => { setText(e.target.value) }
+      const handleSearch = () => { setSearch(text) }
+
+      const matched = useMemo(() => {
+        log()
+        return fn()
+      }, [search])
+
+      r()
+      return (
+        <>
+          <input role="text" value={text} onChange={handleType} />
+          <button role="search" onClick={handleSearch}>Search</button>
+          {matched}          
+        </>
+      )
+    }    
+    
+    const log = jest.fn(), r = jest.fn()
+    const _t = () => screen.getByRole('text')
+    const _s = () => screen.getByRole('search')
+
+    render(<Title log={log} r={r} />)
+    await waitFor(() => expect(r).toHaveReturnedTimes(1))
+    expect(log).toHaveReturnedTimes(1)
+    
+    // first typing session
+    fireEvent.change(_t(), { target: { value: "k" } })
+    fireEvent.change(_t(), { target: { value: "kk" } })
+    fireEvent.change(_t(), { target: { value: "kkk" } })
+    fireEvent.change(_t(), { target: { value: "kkkk" } })
+    await waitFor(() => expect(r).toHaveReturnedTimes(5))
+    expect(log).toHaveReturnedTimes(1)
+
+    // first search click
+    fireEvent.click(_s())
+    await waitFor(() => expect(r).toHaveReturnedTimes(6))
+    expect(log).toHaveReturnedTimes(2)
+
+    // second typing session
+    fireEvent.change(_t(), { target: { value: "k" } })
+    fireEvent.change(_t(), { target: { value: "kk" } })
+    await waitFor(() => expect(r).toHaveReturnedTimes(8))
+    expect(log).toHaveReturnedTimes(2)
+    
+    // second search click
+    fireEvent.click(_s())
+    await waitFor(() => expect(r).toHaveReturnedTimes(9))
+    expect(log).toHaveReturnedTimes(3)
+  })
+
+  test('debounce keyup without useMemo', async () => {
+    const Title = ({ log, r }) => {
+      const [text, setText] = React.useState('')
+      const [matched, setMatched] = useState()
+
+      const handleType = e => { 
+        const search = e.target.value
+        setText(search)
+        log()
+        setMatched(fn(search))
+      }
+
+      r()
+      return (
+        <>
+          <input role="text" value={text} onChange={handleType} />
+          {matched}          
+        </>
+      )
+    }    
+    
+    const log = jest.fn(), r = jest.fn()
+    const _t = () => screen.getByRole('text')
+
+    render(<Title log={log} r={r} />)
+    await waitFor(() => expect(r).toHaveReturnedTimes(1))
+    expect(log).toHaveReturnedTimes(0)
+    
+    // first typing session
+    fireEvent.change(_t(), { target: { value: "k" } })
+    fireEvent.change(_t(), { target: { value: "kk" } })
+    fireEvent.change(_t(), { target: { value: "kkk" } })
+    fireEvent.change(_t(), { target: { value: "kkkk" } })
+    await waitFor(() => expect(r).toHaveReturnedTimes(5))
+    expect(log).toHaveReturnedTimes(4)
+
+    // second typing session
+    fireEvent.change(_t(), { target: { value: "k" } })
+    fireEvent.change(_t(), { target: { value: "kk" } })
+    await waitFor(() => expect(r).toHaveReturnedTimes(7))
+    expect(log).toHaveReturnedTimes(6)
+  })
+})
